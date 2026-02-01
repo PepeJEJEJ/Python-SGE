3e56b22f8bb7f98c352194d3e07fc7f148498f22
PepeJEJEJ <jah0005@alu.medac.es>
2026-02-01 13:54:41 +0100

    Update cs11.py

--- Codigos/PythonCasa/cs11.py
+++ Codigos/PythonCasa/cs11.py
@@ -1,7 +1,14 @@
+#Escribe un programa en Python que pida al usuario su edad y muestre un mensaje indicando
+# si es mayor o menor de edad.
+#Requisitos:
+#Pedir la edad por teclado.
+#Convertirla a número.
+#Mostrar:
+# "Eres mayor de edad" si tiene 18 o más.
+# "Eres menor de edad" si tiene menos de 18.
 
 edad = int(input("¿Tu edad? "))
 if edad<18:
-    print("es menor de edad")
+    print("eres menor de edad")
 elif edad>18:
-    print("es mayor de edad")
-
+    print("eres mayor de edad")
