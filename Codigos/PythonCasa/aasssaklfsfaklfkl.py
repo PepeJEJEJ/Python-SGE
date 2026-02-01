41072f5fec2e72c2478fc805d773717c8eb0a9a8
PepeJEJEJ <jah0005@alu.medac.es>
2026-02-01 13:49:46 +0100

    Add two Python scripts for user interaction and age check
    Added 'aasssaklfsfaklfkl.py', a script with various user prompts, list processing, and loops, and 'cs11.py', a simple script to check if a user is a minor or adult based on input age.

---
+++ Codigos/PythonCasa/aasssaklfsfaklfkl.py
@@ -0,0 +1,44 @@
+nombre = input("¿Cómo te llamas? ")
+print("Hola,", nombre)
+
+habil = input("¿Tienes alguna habilidad? ")
+print(habil, ", Suena Genial, a Ver Si Algun dia me Haces una Demostracion")
+
+gusto = input("¿Te Gusta Algo? ")
+print(gusto, ", A mi tambien me gustan")
+
+odio = input("¿Odias Algo? ")
+print(odio, ", Yo tambien los Odio")
+
+edad = int(input("Introduce tu edad: "))
+for edad in range(0, 42141):
+    if edad % 67 == 0 | edad % 67 == 0:
+        print(edad)
+
+lista = [1, 2, "hacienda", 5, 9.9, 6, "KK"]
+print(lista)
+enteros = []
+
+for elemento in lista:
+    if type(elemento) == int:
+        enteros.append(elemento)
+
+mayor = max(enteros)
+menor = min(enteros)
+
+print("Enteros encontrados:", enteros)
+print("Mayor entero:", mayor)
+print("Menor entero:", menor)
+
+contador = 1
+while contador <= 1000:
+    print(contador)
+    contador += 1
+print("La ejecución ha terminado.")
+
+print(
+    "Makunga fue el principal antagonista de Madagascar 2, es el segundo animal mas sensual del universo, solo siendo superado por Mono de Kung Fu Panda y Moto Moto. Makunga formaba parte de las Gemas de Cristal pero se dio cuenta que solo era una secta donde lo unico que valía era coger y fumar crack, asi que se salio. Poco despues conocio a la esposa de Zuba, con quien tuvo relaciones sexuales que era inevitable por los Poderes Sexuales de Makunga, pero no se cuidaron y nacio Alex, Zuba a guardado el Secreto de su encuentro con Makunga y sería algo muy común con sus Examantes, al igual que sucedería con sus siguientes Amantes como AMLO o Milei, Makunga es parte de el grupo creativo de Novagecko (ahora coinbench), Hincha de Wachipato y tambien Memedroider de corazón, es responsable del ataque del 9/11, la muerte de Fidel Castro y sembro explosivos en la Linea 12 del metro de la CDMX que termino causando un terrible accidente, tambien es conocido por haber incendiado un planeta (despues llamado Sol), es buscado en 77 paises por crímenes como poseedor de CP, Sustancias Ilícitas, los Papeles de las Malvinas, etc. y Makunga a menudo se le ve por las calles de Buenos Aires vendiendo autopartes robadas.")
+
+for x in range(0, 101):
+    if x % 5 == 0:
+        print(x)
---
+++ Codigos/PythonCasa/cs11.py
@@ -0,0 +1,7 @@
+
+edad = int(input("¿Tu edad? "))
+if edad<18:
+    print("es menor de edad")
+elif edad>18:
+    print("es mayor de edad")
+
